ec637775e6a17f32673c07ad8b4b4b22341af22b
rafihabibi <68018633+rafihabibi@users.noreply.github.com>
2026-02-27 20:57:48 +0700

    Update minitask12.js

--- minitask12.js
+++ minitask12.js
@@ -11,3 +11,41 @@ function lowerCaseManual(teksKecil){
   }
   return hurufKecil;
 }
+
+function prosesThenEmail() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+    .then((respon) => {
+        return respon.json();
+    })
+    .then((dataArrayEmail) => {
+        let dataEmail = [];
+        for(let i = 0; i < dataArrayEmail.length;i++) {
+            let emailUsers = dataArrayEmail[i].email;
+            let hurufEmailKecil = lowerCaseManual(emailUsers);
+            dataEmail.push(hurufEmailKecil);
+            
+        }
+    })
+}
+
+async function prosesAsyncEmail() {
+    try {
+        const respon = await fetch("https://jsonplaceholder.typicode.com/users");
+        const users = await respon.json()
+        
+        let emailArray = [];
+        
+        for(let i = 0; i < users.length;i++) {
+            
+            let emailUsers = users[i].email;
+            let lowerEmail = lowerCaseManual(emailUsers);
+            emailArray.push(lowerEmail);
+        }
+      console.log(emailArray);
+    }catch (error){
+        console.error("Terjadi masalah ketika mengambil data")};
+}
+
+prosesThenEmail();
+prosesAsyncEmail();
+
